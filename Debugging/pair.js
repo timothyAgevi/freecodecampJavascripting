fb2b461634b640df58322631fb7640378d6ac87b
timothyAgevi <timothyagevi@gmail.com>
2021-06-20 17:20:06 +0300

    pseudocode

--- Debugging/pair.js
+++ Debugging/pair.js
@@ -1,2 +1,8 @@
 // instruction
     //Fix the two pair errors in the code. 
+
+// pseudocode
+    // Your code should fix the missing piece of the array.
+
+    // Your code should fix the missing piece of the .reduce() 
+    //  The console output should show that Sum of array values is: 6
