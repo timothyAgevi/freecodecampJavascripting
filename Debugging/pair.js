e951952330ab2e3858f230139e1b1b748c26c2ed
timothyAgevi <timothyagevi@gmail.com>
2021-06-20 17:48:20 +0300

    complete pair.js

--- Debugging/pair.js
+++ Debugging/pair.js
@@ -7,6 +7,6 @@
     // Your code should fix the missing piece of the .reduce() 
     //  The console output should show that Sum of array values is: 6
 
-    let myArray = [1, 2, 3]
+    let myArray = [1, 2, 3] ;
         let arraySum = myArray.reduce((previous, current )=> previous + current)
         console.log(`Sum of array values is: ${arraySum}`)   
