b366910c594e84d60ec05f02a936dba040b1b87a
timothyAgevi <timothyagevi@gmail.com>
2021-06-20 20:08:56 +0300

    instruction

--- Debugging/OBOE.js
+++ Debugging/OBOE.js
@@ -3,3 +3,7 @@
 //notes
     // If you try to access an index equal to the length, the program may throw an 
     // "index out of range" reference error or print undefined.
+
+//instructions
+    // Fix the two indexing errors in the following function 
+    // so all the numbers 1 through 5 are printed to the console.
