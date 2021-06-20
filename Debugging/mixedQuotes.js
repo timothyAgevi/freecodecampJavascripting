bd698dc479b13d3d62411eff49fe4611e61574aa
timothyAgevi <timothyagevi@gmail.com>
2021-06-20 18:10:34 +0300

    solution using different quotes

--- Debugging/mixedQuotes.js
+++ Debugging/mixedQuotes.js
@@ -14,3 +14,5 @@
  let innerHtml = "<p>Click here to <a href=\"#Home\">return home</a></p>";
 console.log(innerHtml);
      
+let innerHtml = "<p>Click here to <a href='#Home'>return home</a></p>";
+console.log(innerHtml);
