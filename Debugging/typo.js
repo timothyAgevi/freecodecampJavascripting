9a38f5e0f5ac204ff953a6b1389c48ac0b6e4d2c
timothyAgevi <timothyagevi@gmail.com>
2021-06-20 17:03:01 +0300

    template code

--- Debugging/typo.js
+++ Debugging/typo.js
@@ -5,3 +5,7 @@
     //note
         // avaScript variable and function names are case-sensitive.
 
+        let receivables = 10;
+let payables = 8;
+let netWorkingCapital = recievables - payable;
+console.log(`Net working capital is: ${netWorkingCapital}`);
