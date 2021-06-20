7ca860d134557bef8b2f51369938170bf57f2ccb
timothyAgevi <timothyagevi@gmail.com>
2021-06-20 17:02:17 +0300

    notes

--- Debugging/typo.js
+++ Debugging/typo.js
@@ -1,3 +1,7 @@
 // instruction
     // Fix the two spelling errors in the code
     //  so the netWorkingCapital calculation works.
+
+    //note
+        // avaScript variable and function names are case-sensitive.
+
