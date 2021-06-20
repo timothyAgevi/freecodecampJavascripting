b71166e1a06fde21d4cba7fb5328fcb10c95dca3
timothyAgevi <timothyagevi@gmail.com>
2021-06-20 16:46:32 +0300

    notes

--- Debugging/typeof.js
+++ Debugging/typeof.js
@@ -2,4 +2,10 @@
     // Add two console.log() statements to check the typeof 
     //   each of the two variables seven and three in the code.
 
+ //notes
+    // JavaScript recognizes six primitive (immutable) data types: 
+    // Boolean, Null, Undefined, Number, String, and Symbol(new with ES6)
+    //  and one type for mutable items: Object. 
+    // Note that in JavaScript, arrays are technically a type of object.   
+
     
