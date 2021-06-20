035261274083a19c6dcb96ab240dfec377b1beec
timothyAgevi <timothyagevi@gmail.com>
2021-06-20 18:46:14 +0300

    notes

--- Debugging/equality.js
+++ Debugging/equality.js
@@ -1,3 +1,7 @@
 // instruction
     // Fix the condition so the program runs the right branch, 
     // and the appropriate value is assigned to result.
+
+//note
+// the triple === tests for strict equality, 
+// meaning both value and type are the same).
