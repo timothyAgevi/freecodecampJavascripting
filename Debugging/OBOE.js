982675035c0ef38cde7fb8dfbbb81a5b1b1aed7e
timothyAgevi <timothyagevi@gmail.com>
2021-06-20 20:09:46 +0300

    template code

--- Debugging/OBOE.js
+++ Debugging/OBOE.js
@@ -7,3 +7,15 @@
 //instructions
     // Fix the two indexing errors in the following function 
     // so all the numbers 1 through 5 are printed to the console.
+
+    function countToFive() {
+        let firstFive = "12345";
+        let len = firstFive.length;
+        // Only change code below this line
+        for (let i = 1; i <= len; i++) {
+        // Only change code above this line
+          console.log(firstFive[i]);
+        }
+      }
+      
+      countToFive();
