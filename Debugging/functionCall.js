ddad8de88b5b1c998e38b5d035038a1a18978a0b
timothyAgevi <timothyagevi@gmail.com>
2021-06-20 19:19:54 +0300

    template code

--- Debugging/functionCall.js
+++ Debugging/functionCall.js
@@ -8,4 +8,13 @@
     // }
     // let varOne = myFunction;
     // let varTwo = myFunction();
-    // Here varOne is the function myFunction, and varTwo is the string You rock!.    
+    // Here varOne is the function myFunction, and varTwo is the string You rock!.
+    
+    function getNine() {
+        let x = 6;
+        let y = 3;
+        return x + y;
+      }
+      
+      let result = getNine;
+      console.log(result);   
