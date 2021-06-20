25e04cf6c145373c4d9d2a950f73c024af746496
timothyAgevi <timothyagevi@gmail.com>
2021-06-20 20:06:04 +0300

    notes

--- Debugging/OBOE.js
+++ Debugging/OBOE.js
@@ -1,1 +1,5 @@
 //  OBOE - of by one error
+
+//notes
+    // If you try to access an index equal to the length, the program may throw an 
+    // "index out of range" reference error or print undefined.
