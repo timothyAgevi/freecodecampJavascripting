0083da371922a55439e914be1d39238d8ec6647e
timothyAgevi <timothyagevi@gmail.com>
2021-06-20 19:31:47 +0300

    notes

--- Debugging/arguments.js
+++ Debugging/arguments.js
@@ -1,3 +1,7 @@
 // instruction
     // The function raiseToPower raises a base to an exponent. Unfortunately, it's not called properly -
     //  fix the code so the value of power is the expected 8.
+    
+// note
+    // upply all required arguments, 
+    // in the proper order to avoid these issues.
