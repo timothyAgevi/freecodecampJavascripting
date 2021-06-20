cabe5a8f4e3151cf80c228dfc6f626d2da268b35
timothyAgevi <timothyagevi@gmail.com>
2021-06-20 13:57:10 +0300

    note

--- Debugging/browserConsole.js
+++ Debugging/browserConsole.js
@@ -1,3 +1,7 @@
 // instruction
     // First, use console.log to log the output variable.
     //  Then, use console.clear to clear the browser console.
+
+// note
+    // Note: console.logs inside functions are printed to the freeCodeCamp console whenever those functions are called. 
+    // This can help debugging functions that are called during testing.
