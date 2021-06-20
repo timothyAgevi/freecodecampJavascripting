546e1c3fffe690ef7da444bbfebc9fcc64ba6056
timothyAgevi <timothyagevi@gmail.com>
2021-06-20 13:34:28 +0300

    pseudocode

--- Debugging/valueVariable.js
+++ Debugging/valueVariable.js
@@ -1,2 +1,7 @@
-// Use the console.log() method to print the value
-//  of the variable a where noted in the code.
+// instruction
+      // Use the console.log() method to print the value
+    //  of the variable a where noted in the code.
+
+//pseudocode
+    // You can find Developer tools 
+    // in your Chrome's menu or Web Console in Firefox's menu.
