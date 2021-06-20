a9f4adea0fc1117cf97cbe8a3d7b87e17c8b349e
timothyAgevi <timothyagevi@gmail.com>
2021-06-20 13:36:09 +0300

    template code

--- Debugging/valueVariable.js
+++ Debugging/valueVariable.js
@@ -5,3 +5,13 @@
 //pseudocode
     // You can find Developer tools 
     // in your Chrome's menu or Web Console in Firefox's menu.
+
+    // template code
+    let a = 5;
+    let b = 1;
+    a++;
+    // Only change code below this line
+    
+    
+    let sumAB = a + b;
+    console.log(sumAB);
