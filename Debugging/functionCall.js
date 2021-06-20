f028dc907fb0c906f384ff72a561f1bb5a1c50bd
timothyAgevi <timothyagevi@gmail.com>
2021-06-20 19:17:14 +0300

    notes

--- Debugging/functionCall.js
+++ Debugging/functionCall.js
@@ -1,3 +1,11 @@
 // instruction
     // Fix the code so the variable result is set 
     // to the value returned from calling the function getNine.
+    
+//notes
+    // function myFunction() {
+    //     return "You rock!";
+    // }
+    // let varOne = myFunction;
+    // let varTwo = myFunction();
+    // Here varOne is the function myFunction, and varTwo is the string You rock!.    
