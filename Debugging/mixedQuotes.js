b231018b7c70b3fd60639088ea2c136083f594cc
timothyAgevi <timothyagevi@gmail.com>
2021-06-20 17:56:58 +0300

    instruction

---
+++ Debugging/mixedQuotes.js
@@ -0,0 +1,4 @@
+// instruction
+    // Fix the string so it either uses different quotes 
+    // for the href value, or escape the existing ones. 
+    // Keep the double quote marks around the entire string.
