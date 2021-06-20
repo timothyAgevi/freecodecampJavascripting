74afa5464aac18593b7d901ddad6b08a5f4a9a0c
timothyAgevi <timothyagevi@gmail.com>
2021-06-20 23:05:06 +0300

    instruction

---
+++ Debugging/2dArray.js
@@ -0,0 +1,3 @@
+// instruction
+    // The following function is supposed to create a two-dimensional array with m rows and n columns of zeroes. Unfortunately, it's not producing the expected output because the row variable isn't being reinitialized (set back to an empty array) in the outer loop. Fix the code so it returns a correct 3x2 array of zeroes,
+    //  which looks like [[0, 0], [0, 0], [0, 0]].
