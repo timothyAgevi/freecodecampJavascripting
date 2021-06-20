34f4c12632cf477ec7e32184816f2318d2e97677
timothyAgevi <timothyagevi@gmail.com>
2021-06-20 18:02:41 +0300

    notes

--- Debugging/mixedQuotes.js
+++ Debugging/mixedQuotes.js
@@ -2,3 +2,14 @@
     // Fix the string so it either uses different quotes 
     // for the href value, or escape the existing ones. 
     // Keep the double quote marks around the entire string.
+
+//notes
+    // const grouchoContraction = "I've had a wonderful evening,but this wasn't it.";
+    // const quoteInString = "Groucho Marx once said 'Quote me as saying I was mis-quoted.'";
+    // const uhOhGroucho = 'I've had a perfectly wonderful evening, but this wasn't it.'; 
+    
+    // example 3 above is imcorrect insteaD  would use code below:
+    //   const allSameQuotes = 'I\'ve had a perfectly wonderful evening, but this wasn\'t it.';
+    
+    
+     
