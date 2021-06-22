51b2b84df20e10919e64ed6e47968efa8dd542a4
timothyAgevi <timothyagevi@gmail.com>
2021-06-22 17:40:09 +0300

    template code

--- infiniteLoop.js
+++ infiniteLoop.js
@@ -2,3 +2,9 @@
     // Fix the comparison operator in the
     //terminal condition so the loop only runs 
     // for i less than or equal to 4.
+
+    function myFunc() {
+        for (let i = 1; i != 4; i += 2) {
+          console.log("Still going!");
+        }
+      }
