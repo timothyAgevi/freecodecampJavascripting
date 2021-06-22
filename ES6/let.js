57b15c95f4768fb451a7682d75e5177fd6d7cfa4
timothyAgevi <timothyagevi@gmail.com>
2021-06-22 18:09:11 +0300

    template code

--- ES6/let.js
+++ ES6/let.js
@@ -1,2 +1,13 @@
 // instruction
 //    Update the code so it only uses the let keyword. 
+
+var catName;
+var quote;
+function catTalk() {
+  "use strict";
+
+  catName = "Oliver";
+  quote = catName + " says Meow!";
+
+}
+catTalk();
