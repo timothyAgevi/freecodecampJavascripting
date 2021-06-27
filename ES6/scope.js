ed5970cac2bdfd114d437e9df47d30727992081b
timothyAgevi <timothyagevi@gmail.com>
2021-06-27 16:55:44 +0300

    instruction

--- ES6/scope.js
+++ ES6/scope.js
@@ -1,2 +1,2 @@
-// Fix the code so that i declared in the if statement is a separate variable than i declared in the first line of t
-//  Be certain not to use the var keyword anywhere in your code.
+//Fix the code so that i declared in the if statement is a separate variable than i declared in the first line of
+//Be certain not to use the var keyword anywhere in your code.
