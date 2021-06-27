454fa62821406e83982670a9e5a30c04d675b267
timothyAgevi <timothyagevi@gmail.com>
2021-06-27 17:06:36 +0300

    template code

--- ES6/scope.js
+++ ES6/scope.js
@@ -1,2 +1,12 @@
 //Fix the code so that i declared in the if statement is a separate variable than i declared in the first line of
 //Be certain not to use the var keyword anywhere in your code.
+
+function checkScope() {
+    var i = 'function scope';
+    if (true) {
+      j = 'block scope';
+      console.log('Block scope i is: ', j);
+    }
+    console.log('Function scope i is: ', i);
+    return i;
+  }
