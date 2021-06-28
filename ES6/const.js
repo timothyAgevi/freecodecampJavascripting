f1f298b12104cedf6482736dc163e88327cd5033
timothyAgevi <timothyagevi@gmail.com>
2021-06-28 12:40:51 +0300

    instruction

---
+++ ES6/const.js
@@ -0,0 +1,8 @@
+//instruction
+// Change the code so that all variables
+//  are declared using let or const.
+//   Use let when you want the variable to change,
+//     and const when you want the variable to remain constant.
+//    Also, rename variables declared with const to conform to common practices, 
+// meaning constants should be in all caps.
+
