38e587da90055d653485e4eda847c7943346f779
timothyAgevi <timothyagevi@gmail.com>
2021-06-28 19:29:33 +0300

    emplate code

--- ES6/arrowFunction.js
+++ ES6/arrowFunction.js
@@ -1,2 +1,6 @@
 // Rewrite the function assigned to the variable magic which returns a new Date() to use arrow function syntax.
 //  Also, make sure nothing is defined using the keyword var.
+
+var magic = function() {
+    return new Date();
+  };
