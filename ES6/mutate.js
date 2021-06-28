51135b74c97db6f35089737fcf71b23bfef6062a
timothyAgevi <timothyagevi@gmail.com>
2021-06-28 15:19:12 +0300

    mutate array

--- ES6/mutate.js
+++ ES6/mutate.js
@@ -11,6 +11,9 @@ function editInPlace() {
   // Only change code below this line
 
   // Using s = [2, 5, 7] would be invalid
+  s[0] = 2;
+  s[1] = 5;
+  s[2] = 7;
 
   // Only change code above this line
 }
