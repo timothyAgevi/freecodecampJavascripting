15053d5911d6df1c4b296854a0c36869af287176
timothyAgevi <timothyagevi@gmail.com>
2021-06-28 12:44:17 +0300

    template code

--- ES6/const.js
+++ ES6/const.js
@@ -6,3 +6,17 @@
 //    Also, rename variables declared with const to conform to common practices, 
 // meaning constants should be in all caps.
 
+function printManyTimes(str) {
+
+    // Only change code below this line
+  
+    var sentence = str + " is cool!";
+    for (var i = 0; i < str.length; i+=2) {
+      console.log(sentence);
+    }
+  
+    // Only change code above this line
+  
+  }
+  printManyTimes("freeCodeCamp");
+
