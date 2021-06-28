d68f90f8287d792c984f96197dda8ef27a6fc74b
timothyAgevi <timothyagevi@gmail.com>
2021-06-28 15:08:35 +0300

    instruction

--- ES6/mutate.js
+++ ES6/mutate.js
@@ -1,3 +1,7 @@
 //notes
 // by using const an
 //  array can **NOT** be reassigned but can still be mutated 
+
+// instruction
+// An array is declared as const s = [5, 7, 2]. 
+// Change the array to [2, 5, 7] 
