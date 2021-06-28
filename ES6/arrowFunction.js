050f16bff84c7b9be19808b3e39f46cc17762062
timothyAgevi <timothyagevi@gmail.com>
2021-06-28 19:39:45 +0300

    change let to const

--- ES6/arrowFunction.js
+++ ES6/arrowFunction.js
@@ -1,7 +1,7 @@
 // Rewrite the function assigned to the variable magic which returns a new Date() to use arrow function syntax.
 //  Also, make sure nothing is defined using the keyword var.
 
-let magic = ()=> {
+const magic = ()=> {
     return new Date();
   };
 
