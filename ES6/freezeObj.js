680670bdeabf4c7cdff5a0c0e2872f1e301334c8
timothyAgevi <timothyagevi@gmail.com>
2021-06-28 16:37:45 +0300

    instruction

---
+++ ES6/freezeObj.js
@@ -0,0 +1,5 @@
+// instruction
+// use Object.freeze to prevent mathematical constants
+//  from changing. You need to freeze the MATH_CONSTANTS object
+//   so that no one is
+//  able to alter the value of PI, add,or delete properties.
