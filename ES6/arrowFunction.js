f48abe5c0cd501dc13b1a2ed64f163aa24dd16e8
timothyAgevi <timothyagevi@gmail.com>
2021-06-28 19:35:15 +0300

    guide

--- ES6/arrowFunction.js
+++ ES6/arrowFunction.js
@@ -1,6 +1,13 @@
 // Rewrite the function assigned to the variable magic which returns a new Date() to use arrow function syntax.
 //  Also, make sure nothing is defined using the keyword var.
 
-var magic = function() {
+let magic = ()=> {
     return new Date();
   };
+
+//   You should replace the var keyword.
+
+// magic should be a constant variable (by using const).
+
+// Passed
+// magic should be a function.
