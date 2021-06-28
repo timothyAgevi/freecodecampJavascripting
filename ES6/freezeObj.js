082b0e40578ba358636977697c2e328363ef2be9
timothyAgevi <timothyagevi@gmail.com>
2021-06-28 16:38:33 +0300

    template code

--- ES6/freezeObj.js
+++ ES6/freezeObj.js
@@ -3,3 +3,20 @@
 //  from changing. You need to freeze the MATH_CONSTANTS object
 //   so that no one is
 //  able to alter the value of PI, add,or delete properties.
+
+function freezeObj() {
+    const MATH_CONSTANTS = {
+      PI: 3.14
+    };
+    // Only change code below this line
+  
+  
+    // Only change code above this line
+    try {
+      MATH_CONSTANTS.PI = 99;
+    } catch(ex) {
+      console.log(ex);
+    }
+    return MATH_CONSTANTS.PI;
+  }
+  const PI = freezeObj();
