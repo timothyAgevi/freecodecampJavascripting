e82162943cb7d5a45cacd78f8ba05b6190802f53
timothyAgevi <timothyagevi@gmail.com>
2021-06-28 15:09:13 +0300

    template code

--- ES6/mutate.js
+++ ES6/mutate.js
@@ -5,3 +5,13 @@
 // instruction
 // An array is declared as const s = [5, 7, 2]. 
 // Change the array to [2, 5, 7] 
+
+const s = [5, 7, 2];
+function editInPlace() {
+  // Only change code below this line
+
+  // Using s = [2, 5, 7] would be invalid
+
+  // Only change code above this line
+}
+editInPlace();
